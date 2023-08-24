3ac33a452045f2bede8439aad342fb426eff73df
abdullaalsulaiti <asulait2@andrew.cmu.edu>
2023-08-24 20:12:25 +0300

    commit

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,8 +1,7 @@
-// Endpoint for querying the fibonacci numbers
+import fibonacci from "./fib";
+import { Request, Response } from "express";
 
-const fibonacci = require("./fib");
-
-export default (req, res) => {
+export default (req : Request, res : Response) => {
   const { num } = req.params;
 
   const fibN = fibonacci(parseInt(num));
@@ -13,4 +12,5 @@ export default (req, res) => {
   }
 
   res.send(result);
+  
 };
